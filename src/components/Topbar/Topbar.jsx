c28fa2061286350cae21c5f4342a4ccac74e1afb
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-05-05 22:07:37 +0530

    Finished intro page

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -3,15 +3,12 @@ import './topbar.scss';
 function Topbar() {
   return (
     <div class = "navbar">
-        <div className = "nav-icon">
-            <h3>Rohith Nambiar</h3>
-        </div>
-        <div className = "nav-links">
-            <ul>
-                <li><a href = "#About" class = "link">ABOUT</a></li>
-                <li><a href = "#Projects" class = "link">PROJECTS</a></li>
-                <li><a href = "#Contact" class = "link">CONTACT</a></li>
-            </ul>
+        <div class="menu">
+          <div class="label">Navbar</div>
+          <div class="spacer"></div>
+          <div class="item"><span><a href = "#About" class = "link">ABOUT</a></span></div>
+          <div class="item"><span><a href = "#Projects" class = "link">PROJECTS</a></span></div>
+          <div class="item"><span><a href = "#Contact" class = "link">CONTACT</a></span></div>
         </div>
     </div>
   )
