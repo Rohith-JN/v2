236cbb64788325b746d874e0715804d422959b5f
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-05-28 11:18:39 +0530

    Fixed Hamburger-menu bug

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,6 +1,24 @@
 import './topbar.scss';
 
 function Topbar() {
+  const timeout = () => {
+    setTimeout(() => {
+      if (window.location.pathname === '/') {
+        uncheckAll();
+      }
+    }, 600);
+  };
+
+  function check(checked = true) {
+    const checkboxes = document.querySelectorAll('input.checkbox');
+    checkboxes.forEach((checkbox) => {
+      checkbox.checked = checked;
+    });
+  }
+
+  function uncheckAll() {
+    check(false);
+  }
   return (
     <div className="nav">
       <div className="navbar">
@@ -35,6 +53,36 @@ function Topbar() {
               </a>
             </span>
           </div>
+        </div>
+      </div>
+      <div className="nav-menu">
+        <div id="menuToggle">
+          <input type="checkbox" className="checkbox" />
+          <span></span>
+          <span></span>
+          <span></span>
+          <ul id="menu">
+            <li>
+              <a href="#Intro" className="link" onClick={timeout}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#About" className="link" onClick={timeout}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#Projects" className="link" onClick={timeout}>
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#Contact" className="link" onClick={timeout}>
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
