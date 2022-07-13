697b1fbf994aae176b1db7f69ff2d66be21152f3
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-07-13 09:06:46 +0530

    Added meta-tags

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from './components/About/About';
 import './app.scss';
 import React, { useEffect } from 'react';
 import Load from './components/Loading-screen/Load';
+import DocumentMeta from 'react-document-meta';
 
 export default function App() {
   const [isLoading, setIsLoading] = React.useState(true);
@@ -19,21 +20,36 @@ export default function App() {
     return () => window.removeEventListener('load', handleLoading);
   }, []);
 
+  const meta = {
+    title: 'Rohith J Nambiar',
+    description:
+      'Hi I am Rohith Nambiar a junior developer aspiring to be a full-stack web developer, specializing in React.js, Python, APIs',
+    canonical: 'https://rohithnambiar.in/',
+    meta: {
+      charset: 'utf-8',
+      name: {
+        keywords: 'portfolio, about, projects, contact',
+      },
+    },
+  };
+
   return (
-    <div>
-      {isLoading ? (
-        <Load />
-      ) : (
-        <div className="app">
-          <Topbar />
-          <div className="parts">
-            <Intro />
-            <About />
-            <Projects />
-            <Contact />
+    <DocumentMeta {...meta}>
+      <div>
+        {isLoading ? (
+          <Load />
+        ) : (
+          <div className="app">
+            <Topbar />
+            <div className="parts">
+              <Intro />
+              <About />
+              <Projects />
+              <Contact />
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </DocumentMeta>
   );
 }
